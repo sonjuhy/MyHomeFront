8deac2613e8c6800024e756dbfb9423cac56ff47
minyoung <minyoung99311@gmail.com>
2024-01-14 15:28:58 +0900

    :lipstick: DefaultTheme Color Setting
    DefaultTheme Color Setting

--- my-home/src/styles/DefaultTheme.ts
+++ my-home/src/styles/DefaultTheme.ts
@@ -2,15 +2,13 @@ import { DefaultTheme } from 'styled-components';
 
 const theme: DefaultTheme = {
   colors: {
-    main: '#F36969',
-    light: '#9CC0C4',
-    dark: '#3D5A61',
-    white: '#F3F8F8',
-    black: '#1E2B2E',
+    main: '#5CD8E0',
+    light: '#F6FAFD',
+    dark: '#4AADB3',
+    white: '#FFFFFF',
+    black: '#222529',
     lightgray: '#E4E7EC',
-    gray: 'rgba(0, 0, 0, 0.50)',
-    night: '#000020',
-    menu: '#E0ECED',
+    gray: '#585858',
   },
   fonts: {
     lightfont: 'lightfont',
