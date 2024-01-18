e0e0c86f4182ac66d980fd477157c6ed2713a886
minyoung <minyoung99311@gmail.com>
2024-01-18 14:02:07 +0900

    :sparkles: Api 연동
    api 연동

--- my-home/src/api/notice/NoticeGetTopNotice.ts
+++ my-home/src/api/notice/NoticeGetTopNotice.ts
@@ -5,10 +5,6 @@ const NoticeTopGet = async () => {
     const response = await serverAxios({
       method: 'get',
       url: '/notice/getTopNotice',
-      headers: {
-        Authorization:
-          'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJyZWd1bGFyIiwicm9sZXMiOiJzdHJpbmciLCJpYXQiOjE3MDU1MDQ4MzksImV4cCI6MTcwNzY2NDgzOX0.UrTc1AGXJDQnGN9ysbWQIhYqS4MzNs0OfLLdkDqY9yE',
-      },
     });
     return response;
   } catch (error) {
