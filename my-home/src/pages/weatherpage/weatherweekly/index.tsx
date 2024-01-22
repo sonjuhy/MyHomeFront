ded2e93966fa4c944c06521d31cc840a92efa031
minyoung <minyoung99311@gmail.com>
2024-01-22 10:23:03 +0900

    :sparkles: Weather weekly 레이아웃 구현
    weather weekly 레이아웃 구현

--- my-home/src/pages/weatherpage/weatherweekly/index.tsx
+++ my-home/src/pages/weatherpage/weatherweekly/index.tsx
@@ -1,7 +1,122 @@
-import { StyledWeatherWeekly } from './WeatherWeekly.styled';
+import {
+  StyledWeatherWeekly,
+  StyledWeatherWeeklyBold,
+  StyledWeatherWeeklyCard,
+  StyledWeatherWeeklyCardFlex,
+  StyledWeatherWeeklyCardIcon,
+  StyledWeatherWeeklyRegular,
+  StyledWeatherWeeklyTodayMark,
+  StyledWeatherWeeklyTodayMarkTriangle,
+} from './WeatherWeekly.styled';
 
 const WeatherWeekly = () => {
-  return <StyledWeatherWeekly>이번주날씨</StyledWeatherWeekly>;
+  return (
+    <StyledWeatherWeekly>
+      <StyledWeatherWeeklyTodayMarkTriangle />
+      <StyledWeatherWeeklyTodayMark />
+      <StyledWeatherWeeklyBold id="mark">TODAY</StyledWeatherWeeklyBold>
+
+      {/* 나중에 map 돌릴 예정 레이아웃용 */}
+      <StyledWeatherWeeklyCard>
+        <StyledWeatherWeeklyBold id="cardTitle">수요일</StyledWeatherWeeklyBold>
+        <StyledWeatherWeeklyRegular id="cardDate">0122</StyledWeatherWeeklyRegular>
+        <br />
+        <StyledWeatherWeeklyCardFlex>
+          <StyledWeatherWeeklyRegular id="cardDate">min.</StyledWeatherWeeklyRegular>
+          <StyledWeatherWeeklyBold id="cardMax">14ºC</StyledWeatherWeeklyBold>
+        </StyledWeatherWeeklyCardFlex>
+        <StyledWeatherWeeklyCardFlex>
+          <StyledWeatherWeeklyRegular id="cardDate">max.</StyledWeatherWeeklyRegular>
+          <StyledWeatherWeeklyBold id="cardMax">18ºC</StyledWeatherWeeklyBold>
+        </StyledWeatherWeeklyCardFlex>
+        <StyledWeatherWeeklyCardIcon src="/weather/icon/sunny.png" alt="sunny" />
+      </StyledWeatherWeeklyCard>
+      <StyledWeatherWeeklyCard>
+        <StyledWeatherWeeklyBold id="cardTitle">수요일</StyledWeatherWeeklyBold>
+        <StyledWeatherWeeklyRegular id="cardDate">0122</StyledWeatherWeeklyRegular>
+        <br />
+        <StyledWeatherWeeklyCardFlex>
+          <StyledWeatherWeeklyRegular id="cardDate">min.</StyledWeatherWeeklyRegular>
+          <StyledWeatherWeeklyBold id="cardMax">14ºC</StyledWeatherWeeklyBold>
+        </StyledWeatherWeeklyCardFlex>
+        <StyledWeatherWeeklyCardFlex>
+          <StyledWeatherWeeklyRegular id="cardDate">max.</StyledWeatherWeeklyRegular>
+          <StyledWeatherWeeklyBold id="cardMax">18ºC</StyledWeatherWeeklyBold>
+        </StyledWeatherWeeklyCardFlex>
+        <StyledWeatherWeeklyCardIcon src="/weather/icon/sunny.png" alt="sunny" />
+      </StyledWeatherWeeklyCard>
+      <StyledWeatherWeeklyCard>
+        <StyledWeatherWeeklyBold id="cardTitle">수요일</StyledWeatherWeeklyBold>
+        <StyledWeatherWeeklyRegular id="cardDate">0122</StyledWeatherWeeklyRegular>
+        <br />
+        <StyledWeatherWeeklyCardFlex>
+          <StyledWeatherWeeklyRegular id="cardDate">min.</StyledWeatherWeeklyRegular>
+          <StyledWeatherWeeklyBold id="cardMax">14ºC</StyledWeatherWeeklyBold>
+        </StyledWeatherWeeklyCardFlex>
+        <StyledWeatherWeeklyCardFlex>
+          <StyledWeatherWeeklyRegular id="cardDate">max.</StyledWeatherWeeklyRegular>
+          <StyledWeatherWeeklyBold id="cardMax">18ºC</StyledWeatherWeeklyBold>
+        </StyledWeatherWeeklyCardFlex>
+        <StyledWeatherWeeklyCardIcon src="/weather/icon/sunny.png" alt="sunny" />
+      </StyledWeatherWeeklyCard>
+      <StyledWeatherWeeklyCard>
+        <StyledWeatherWeeklyBold id="cardTitle">수요일</StyledWeatherWeeklyBold>
+        <StyledWeatherWeeklyRegular id="cardDate">0122</StyledWeatherWeeklyRegular>
+        <br />
+        <StyledWeatherWeeklyCardFlex>
+          <StyledWeatherWeeklyRegular id="cardDate">min.</StyledWeatherWeeklyRegular>
+          <StyledWeatherWeeklyBold id="cardMax">14ºC</StyledWeatherWeeklyBold>
+        </StyledWeatherWeeklyCardFlex>
+        <StyledWeatherWeeklyCardFlex>
+          <StyledWeatherWeeklyRegular id="cardDate">max.</StyledWeatherWeeklyRegular>
+          <StyledWeatherWeeklyBold id="cardMax">18ºC</StyledWeatherWeeklyBold>
+        </StyledWeatherWeeklyCardFlex>
+        <StyledWeatherWeeklyCardIcon src="/weather/icon/sunny.png" alt="sunny" />
+      </StyledWeatherWeeklyCard>
+      <StyledWeatherWeeklyCard>
+        <StyledWeatherWeeklyBold id="cardTitle">수요일</StyledWeatherWeeklyBold>
+        <StyledWeatherWeeklyRegular id="cardDate">0122</StyledWeatherWeeklyRegular>
+        <br />
+        <StyledWeatherWeeklyCardFlex>
+          <StyledWeatherWeeklyRegular id="cardDate">min.</StyledWeatherWeeklyRegular>
+          <StyledWeatherWeeklyBold id="cardMax">14ºC</StyledWeatherWeeklyBold>
+        </StyledWeatherWeeklyCardFlex>
+        <StyledWeatherWeeklyCardFlex>
+          <StyledWeatherWeeklyRegular id="cardDate">max.</StyledWeatherWeeklyRegular>
+          <StyledWeatherWeeklyBold id="cardMax">18ºC</StyledWeatherWeeklyBold>
+        </StyledWeatherWeeklyCardFlex>
+        <StyledWeatherWeeklyCardIcon src="/weather/icon/sunny.png" alt="sunny" />
+      </StyledWeatherWeeklyCard>
+      <StyledWeatherWeeklyCard>
+        <StyledWeatherWeeklyBold id="cardTitle">수요일</StyledWeatherWeeklyBold>
+        <StyledWeatherWeeklyRegular id="cardDate">0122</StyledWeatherWeeklyRegular>
+        <br />
+        <StyledWeatherWeeklyCardFlex>
+          <StyledWeatherWeeklyRegular id="cardDate">min.</StyledWeatherWeeklyRegular>
+          <StyledWeatherWeeklyBold id="cardMax">14ºC</StyledWeatherWeeklyBold>
+        </StyledWeatherWeeklyCardFlex>
+        <StyledWeatherWeeklyCardFlex>
+          <StyledWeatherWeeklyRegular id="cardDate">max.</StyledWeatherWeeklyRegular>
+          <StyledWeatherWeeklyBold id="cardMax">18ºC</StyledWeatherWeeklyBold>
+        </StyledWeatherWeeklyCardFlex>
+        <StyledWeatherWeeklyCardIcon src="/weather/icon/sunny.png" alt="sunny" />
+      </StyledWeatherWeeklyCard>
+      <StyledWeatherWeeklyCard>
+        <StyledWeatherWeeklyBold id="cardTitle">수요일</StyledWeatherWeeklyBold>
+        <StyledWeatherWeeklyRegular id="cardDate">0122</StyledWeatherWeeklyRegular>
+        <br />
+        <StyledWeatherWeeklyCardFlex>
+          <StyledWeatherWeeklyRegular id="cardDate">min.</StyledWeatherWeeklyRegular>
+          <StyledWeatherWeeklyBold id="cardMax">14ºC</StyledWeatherWeeklyBold>
+        </StyledWeatherWeeklyCardFlex>
+        <StyledWeatherWeeklyCardFlex>
+          <StyledWeatherWeeklyRegular id="cardDate">max.</StyledWeatherWeeklyRegular>
+          <StyledWeatherWeeklyBold id="cardMax">18ºC</StyledWeatherWeeklyBold>
+        </StyledWeatherWeeklyCardFlex>
+        <StyledWeatherWeeklyCardIcon src="/weather/icon/sunny.png" alt="sunny" />
+      </StyledWeatherWeeklyCard>
+    </StyledWeatherWeekly>
+  );
 };
 
 export default WeatherWeekly;
